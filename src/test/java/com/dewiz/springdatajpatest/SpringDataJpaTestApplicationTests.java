43357c5e3cdc0390def500175196c6de99266f65
dewiz <churkin.it@gmail.com>
2023-05-24 18:40:01 +0300

    Initial commit

---
+++ src/test/java/com/dewiz/springdatajpatest/SpringDataJpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dewiz.springdatajpatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
