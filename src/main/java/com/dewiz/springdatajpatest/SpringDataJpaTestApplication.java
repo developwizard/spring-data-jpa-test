896446ea082ac7d73d66d01c052a10364c89cc14
dewiz <churkin.it@gmail.com>
2023-05-24 18:59:23 +0300

    Added hibernate dependency

--- src/main/java/com/dewiz/springdatajpatest/SpringDataJpaTestApplication.java
+++ src/main/java/com/dewiz/springdatajpatest/SpringDataJpaTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDataJpaTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJpaTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaTestApplication.class, args);
+    }
 
 }
